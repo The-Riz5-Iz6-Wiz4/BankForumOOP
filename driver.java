f4d8465ee5b010b1b8c63540acd2de0b9d915626
The-Riz5-Iz6-Wiz4 <ridibuar@gmail.com>
2022-05-16 18:43:40 +0700

    Upload the driver

--- driver.java
+++ driver.java
@@ -1,3 +1,10 @@
 public class driver {
-    
+    public static void main(String[] args) {
+        System.out.println("Welcome to someBank");
+
+        Bank someBank = new  Bank("someBank");
+        boolean state = true;
+
+        Methods.bootUp(someBank, state);
+    }
 }
